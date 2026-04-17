ebf3294bc61c5d0452e1f8d4fdbc63b9845fc121
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-04-17 16:33:25 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+import { listenAuth } from "./core/auth.js";
+import { getRole } from "./core/role.js";
 
+listenAuth(async (user) => {
+
+  if (!user) {
+    if (!location.pathname.includes("login.html")) {
+      location.href = "login.html";
+    }
+    return;
+  }
+
+  const role = await getRole(user.uid);
+
+  if (location.pathname.includes("login.html")) {
+
+    if (role === "admin") location.href = "admin.html";
+    if (role === "worker") location.href = "worker.html";
+    if (role === "kitchen") location.href = "kitchen.html";
+    if (role === "barman") location.href = "barman.html";
+    if (role === "finance") location.href = "finance.html";
+
+  }
+
+});
